fb0f0b5a41a9c6d50fb0861bc5c30bd99dcb3c77
anushak-gif <59634635+anushak-gif@users.noreply.github.com>
2020-04-16 16:17:19 -0400

    Linegraph typo

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-import LineGraph from "./components/LineGraph";
+import LineGraph from "./components/Linegraph";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 class App extends Component {
