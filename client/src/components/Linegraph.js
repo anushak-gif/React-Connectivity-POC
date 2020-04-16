0c0f3ac503f83479e625911410fade40da6aeb57
zmiles17 <zgm_striker17@hotmail.com>
2020-04-16 14:26:19 -0400

    Second Commit

--- client/src/components/Linegraph.js
+++ client/src/components/Linegraph.js
@@ -1,5 +1,75 @@
 import React from "react";
 import { Line } from "react-chartjs-2";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Dropdown from "react-bootstrap/Dropdown";
+
+let totalTests = 0,
+  positiveCases = 0,
+  negativeCases = 0;
+
+const states = [
+  "AL",
+  "AK",
+  "AS",
+  "AZ",
+  "AR",
+  "CA",
+  "CO",
+  "CT",
+  "DE",
+  "DC",
+  "FM",
+  "FL",
+  "GA",
+  "GU",
+  "HI",
+  "ID",
+  "IL",
+  "IN",
+  "IA",
+  "KS",
+  "KY",
+  "LA",
+  "ME",
+  "MH",
+  "MD",
+  "MA",
+  "MI",
+  "MN",
+  "MS",
+  "MO",
+  "MT",
+  "NE",
+  "NV",
+  "NH",
+  "NJ",
+  "NM",
+  "NY",
+  "NC",
+  "ND",
+  "MP",
+  "OH",
+  "OK",
+  "OR",
+  "PW",
+  "PA",
+  "PR",
+  "RI",
+  "SC",
+  "SD",
+  "TN",
+  "TX",
+  "UT",
+  "VT",
+  "VI",
+  "VA",
+  "WA",
+  "WV",
+  "WI",
+  "WY",
+];
 
 export default class LineGraph extends React.Component {
   constructor(props) {
@@ -32,17 +102,17 @@ export default class LineGraph extends React.Component {
           label: "Positive",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
+          backgroundColor: "green",
           borderColor: "green",
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
+          pointBorderColor: "green",
+          pointBackgroundColor: "green",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBackgroundColor: "green",
           pointHoverBorderColor: "rgba(220,220,220,1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
@@ -53,17 +123,17 @@ export default class LineGraph extends React.Component {
           label: "Negative",
           fill: false,
           lineTension: 0.1,
-          backgroundColor: "rgba(75,192,192,0.4)",
+          backgroundColor: "red",
           borderColor: "red",
           borderCapStyle: "butt",
           borderDash: [],
           borderDashOffset: 0.0,
           borderJoinStyle: "miter",
-          pointBorderColor: "rgba(75,192,192,1)",
-          pointBackgroundColor: "#fff",
+          pointBorderColor: "red",
+          pointBackgroundColor: "red",
           pointBorderWidth: 1,
           pointHoverRadius: 5,
-          pointHoverBackgroundColor: "rgba(75,192,192,1)",
+          pointHoverBackgroundColor: "red",
           pointHoverBorderColor: "rgba(220,220,220,1)",
           pointHoverBorderWidth: 2,
           pointRadius: 1,
@@ -103,11 +173,13 @@ export default class LineGraph extends React.Component {
         console.log(err);
         return null;
       });
-    console.log(response);
-    response[0].reverse();
-    response[1].reverse();
-    response[2].reverse();
-    response[3].reverse();
+    totalTests = response[0][0];
+    positiveCases = response[1][0];
+    negativeCases = response[2][0];
+    response[0].reverse(); // total
+    response[1].reverse(); // positive
+    response[2].reverse(); // negative
+    response[3].reverse(); // dates
     currentState.datasets[0].data = response[0];
     currentState.datasets[1].data = response[1];
     currentState.datasets[2].data = response[2];
@@ -116,6 +188,40 @@ export default class LineGraph extends React.Component {
   }
 
   render() {
-    return <Line ref={this.chartReference} data={this.state} width={50} height={20} />;
+    return (
+      <Container fluid>
+        <Dropdown>
+          <Dropdown.Toggle variant="success" id="dropdown-basic">
+            STATE
+          </Dropdown.Toggle>
+          <Dropdown.Menu>
+            {states.map((state) => (
+              <Dropdown.Item>{state}</Dropdown.Item>
+            ))}
+          </Dropdown.Menu>
+        </Dropdown>
+        <Row>
+          <Col>
+            <Line
+              ref={this.chartReference}
+              data={this.state}
+              width={50}
+              height={25}
+              options={{ maintainAspectRatio: true }}
+            />
+          </Col>
+          <Col>
+            <Row>Positive Cases</Row>
+            <Row>{positiveCases}</Row>
+            <br />
+            <Row>Negative Cases</Row>
+            <Row>{negativeCases}</Row>
+            <br />
+            <Row>Total Tests</Row>
+            <Row>{totalTests}</Row>
+          </Col>
+        </Row>
+      </Container>
+    );
   }
 }
